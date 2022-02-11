610173b33064363f65f1d9f1601eae8ee7f00139
Rompil <rompil@inbox.ru>
2022-02-11 21:45:12 +0300

    another try

--- solution.go
+++ solution.go
@@ -1,10 +1,12 @@
 package solution
 
 
-import 
-	"github.com/kyokomi/emoji"
+import 	(
+	"github.com/kyokomi/emoji/v2"
+	// "fmt"
+)
 
 
 func GetMessage() string {
-	return emoji.Sprint("Hello :world_map:!")
+	return emoji.Sprint("Hello :world_map: !")
 }
